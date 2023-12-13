becca315e831a6a2d961db4244aec102a847cb41
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-13 10:42:09 +0100

    13/12/23_10:42

--- segundaev/src/Ejercicio1/Tester.java
+++ segundaev/src/Ejercicio1/Tester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        int opcion=0;
+        int opcion=20;
         do {
             System.out.println("1.numero perfecto\n2.piramide\n0.salir");
             opcion = scan.nextInt();
--- segundaev/src/Ejercicio2/Tester.java
+++ segundaev/src/Ejercicio2/Tester.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("1.Iniciales nombre y apellidos\n2.Concatenar dos string\n3.Contar palabras de un String\n4.Cuantas veces aparece String dentro de otro.");
-        int opcion= scan.nextInt();
-        switch(opcion){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
+        int opcion=20;
+        do {
+            System.out.println("1.Iniciales nombre y apellidos\n2.Concatenar dos string\n3.Contar palabras de un String\n4.Cuantas veces aparece String dentro de otro.\n0.Salir");
+            opcion = scan.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }while(opcion!=0);
     }
 }
