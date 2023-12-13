3d28b077d60873753073b43cfb86c748e80eb51e
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-13 09:33:47 +0100

    13/12/23_09:33

--- segundaev/src/Main.java
+++ segundaev/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int suma (int x, int y){
+        int resultado =x+y;
+        return resultado;
+    }
+    public static void resta (){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduzca operandos");
+        int num1=scan.nextInt();
+        int num2=scan.nextInt();
+        System.out.println(num1-num2);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1.sumar\n 2.restar\n 3.multiplicar\n 4.dividir");
+        int opcion =scan.nextInt();
+
+        switch (opcion){
+            case 1:
+                System.out.println("Introduzca operandos");
+                int num1=scan.nextInt();
+                int num2=scan.nextInt();
+                System.out.println(suma(num1,num2));
+                break;
+            case 2:
+                resta();
+                break;
+        }
     }
 }
