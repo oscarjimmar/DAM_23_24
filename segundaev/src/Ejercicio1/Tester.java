905b3323b5908322afd8c458ae72c42050b1e23f
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-13 10:40:35 +0100

    13/12/23_10:40

--- segundaev/src/Ejercicio1/Tester.java
+++ segundaev/src/Ejercicio1/Tester.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("1.numero perfecto\n2.piramide");
-        int opcion= scan.nextInt();
-        switch (opcion){
-            case 1:
-                System.out.println("introduce un numero");
-                int num= scan.nextInt();
-                OperacionesVarias.perfecto(num);
-                break;
-            case 2:
-                OperacionesVarias.piramide();
-                break;
-        }
+        int opcion=0;
+        do {
+            System.out.println("1.numero perfecto\n2.piramide\n0.salir");
+            opcion = scan.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("introduce un numero");
+                    int num = scan.nextInt();
+                    OperacionesVarias.perfecto(num);
+                    break;
+                case 2:
+                    OperacionesVarias.piramide();
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+            }
+        }while(opcion!=0);
     }
 }
---
+++ segundaev/src/Ejercicio2/Tester.java
@@ -0,0 +1,21 @@
+package Ejercicio2;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("1.Iniciales nombre y apellidos\n2.Concatenar dos string\n3.Contar palabras de un String\n4.Cuantas veces aparece String dentro de otro.");
+        int opcion= scan.nextInt();
+        switch(opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
