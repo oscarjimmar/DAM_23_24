773cb6e500a08b4cd4dc94884a6506dd29e0bb68
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-13 12:32:08 +0100

    13/12/23_12:32

--- segundaev/src/Ejercicio2/OperacionesString.java
+++ segundaev/src/Ejercicio2/OperacionesString.java
@@ -1,5 +1,16 @@
 package Ejercicio2;
 
 public class OperacionesString {
-
+    public static String inicial(String x, String y,String j){
+        String str1= String.valueOf(x.charAt(0));
+        String str2= String.valueOf(y.charAt(0));
+        String str3= String.valueOf(j.charAt(0));
+        String fin =str1.concat(str2.concat(str3));
+        return fin;
+    }
+    public static String concatenacion(String x, String y){
+        String str1=x.toUpperCase();
+        String str2=y.toUpperCase();
+        return str1.concat(str2);
+    }
 }
--- segundaev/src/Ejercicio2/Tester.java
+++ segundaev/src/Ejercicio2/Tester.java
@@ -1,24 +1,47 @@
 package Ejercicio2;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Tester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan=new Scanner(System.in);
-        int opcion=20;
+        char opcion=20;
         do {
             System.out.println("1.Iniciales nombre y apellidos\n2.Concatenar dos string\n3.Contar palabras de un String\n4.Cuantas veces aparece String dentro de otro.\n0.Salir");
-            opcion = scan.nextInt();
+            opcion = (char) System.in.read();
+            System.in.read();
             switch (opcion) {
-                case 1:
+                case '1':
+                    System.out.println("Introduce tu nombre");
+                    String nombre= scan.nextLine();
+                    System.out.println("Introduce tu primer apellido");
+                    String apellido1= scan.nextLine();
+                    System.out.println("Introduce tu segundo apellido");
+                    String apellido2=scan.nextLine();
+                    System.out.println(OperacionesString.inicial(nombre,apellido1,apellido2));
                     break;
-                case 2:
+                case '2':
+                    System.out.println("Introduce una frase");
+                    String str1=scan.nextLine();
+                    System.out.println("Introduce otra frase");
+                    String str2=scan.nextLine();
+                    System.out.println(OperacionesString.concatenacion(str1,str2));
                     break;
-                case 3:
+                case '3':
                     break;
-                case 4:
+                case '4':
+                    System.out.println("Introduce una frase.");
+                    String frase= scan.nextLine();
+                    System.out.println("Introduce la palabra que  quieres ver cuanto se repite en la frase");
+                    String [] serie=frase.split(" ");
+
+                    String repeticion;
+
                     break;
+                default:
+                    System.out.println("opcion no valida");
             }
-        }while(opcion!=0);
+        }while(opcion!='0');
     }
 }
