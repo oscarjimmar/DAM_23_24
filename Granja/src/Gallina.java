50be9b38018b6016bac57db275aa185dd38ed1f8
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-18 15:01:47 +0100

    18/12/2023_15:01

--- Granja/src/Gallina.java
+++ Granja/src/Gallina.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Gallina {
     private String nombre;
     private int edad;
     private float peso;
+    private int[]huevosSemana ;
 
     private static final String [] nombres={"Paco","Maksim","Quien","Goku","MartyMcfly"};
     public static String getNombreAleatorio(){
@@ -11,23 +14,32 @@ public class Gallina {
         nombre =getNombreAleatorio();
         peso= (float) (Math.random()*3+1);
         edad= (int) (Math.random()*9+1);
+        huevosSemana=new int[7];
+        for (int i = 0; i < huevosSemana.length; i++) {
+            huevosSemana[i]= (int) (Math.random()*2);
+        }
     }
     public Gallina(String nombre,int edad, float peso){
         this.nombre=nombre;
         this.edad=edad;
         this.peso=peso;
+        huevosSemana=new int[7];
+    }
+    public void huevosDia(int dia, int huevos){
+        huevosSemana[dia-1]=huevos;
+    }
+
+    /*public void mostrarInfo(){
+        System.out.println("Me llamo "+nombre+" tengo "+edad+" años y peso "+peso+" kg");
+    }*/
+    public String toString(){
+        return String.format("Mellamo %s, este es mi peso %.2f y tengo %d años y pongo a la semana \n %s",nombre,peso,edad, Arrays.toString(huevosSemana));
     }
     public String getNombre() {
         return nombre;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-    /*public void mostrarInfo(){
-        System.out.println("Me llamo "+nombre+" tengo "+edad+" años y peso "+peso+" kg");
-    }*/
-    public String toString(){
-        return String.format("Mellamo %s, este es mi peso %.2f y tengo %d años");
     }
     public void setPeso(float peso) {
         this.peso = peso;
@@ -40,5 +52,11 @@ public class Gallina {
     }
     public int getEdad() {
         return edad;
+    }
+    public void setHuevosSemana(int[] huevosSemana) {
+        this.huevosSemana = huevosSemana;
+    }
+    public int[] getHuevosSemana() {
+        return huevosSemana;
     }
 }
