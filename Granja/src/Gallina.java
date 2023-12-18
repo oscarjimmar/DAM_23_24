c73bcc536c99521fdd4e4791c7d0970246b055e5
oscar <oscar.jimenezmartinez@educa.madrid.org>
2023-12-18 14:30:16 +0100

    18/12/2023_14:30

--- Granja/src/Gallina.java
+++ Granja/src/Gallina.java
@@ -1,2 +1,27 @@
 public class Gallina {
+    private String nombre;
+    private int edad;
+    private float peso;
+
+    private static final String [] nombres={"Paco","Maksim","Quien","Goku","MartyMcfly"};
+    public static String getNombreAleatorio(){
+        return nombres[(int) (Math.random()* nombres.length)];
+    }
+    public Gallina(){
+        nombre =getNombreAleatorio();
+    }
+    public Gallina(String nombre,int edad, float peso){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.peso=peso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void mostrarInfo(){
+        System.out.println("Me llamo "+nombre+" tengo "+edad+" años y peso "+peso+" kg");
+    }
 }
--- Granja/src/Main.java
+++ Granja/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("nombre,edad,peso");
+        String name= scan.nextLine();
+        int edad= scan.nextInt();
+        float peso= scan.nextFloat();
+        Gallina random =new Gallina();
+        Gallina gallina=new Gallina();
+        Gallina gallina1=new Gallina(name,edad,peso);
+        gallina.mostrarInfo();
+        gallina1.mostrarInfo();
+        random.mostrarInfo();
     }
 }
