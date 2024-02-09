f37ffd6b14b0138298446e63e5831bd706a75252
OSCARJ <oscar.jimenezmartinez@educa.madrid.org>
2024-02-09 13:09:10 +0100

    1

--- Videoclub/src/Utilidades.java
+++
@@ -1,6 +0,0 @@
-public class Utilidades {
-    public static final String [] titulo={"Spider-man","Good guys","Batman","Vengadores","Kingsman"};
-    public static String getTituloAleatorio(){
-        return titulo[(int) (Math.random()* titulo.length)];
-    }
-}
