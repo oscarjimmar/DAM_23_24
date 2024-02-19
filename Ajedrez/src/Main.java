0799316996a08195dd3949a51117b53930061bc7
Oscar Jimenez <oscar.jimenezmartinez@educa.madrid.org>
2024-02-19 12:05:06 +0100

    19/02/2024

--- Ajedrez/src/Main.java
+++ Ajedrez/src/Main.java
@@ -6,59 +6,119 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Tablero tablero = new Tablero();
         int turno = 0;
-        boolean turn = true;
+        boolean turn = false;
         Juego movi = new Juego(turn);
         while (turno<20) {
-            tablero.pintarTablero();
-            System.out.println("jugada");
-            String jugadadw = scan.nextLine();
-            if (movi.jugada(jugadadw, tablero) != null) {
-                Posicion inicio = movi.jugada(jugadadw, tablero).getPosInicio();
-                Posicion fin = movi.jugada(jugadadw, tablero).getPosFinal();
-                Pieza aux = tablero.DevolverPieza(inicio);
-                if (aux.validoMovimiento(movi.jugada(jugadadw, tablero))) {
-                    if (!tablero.hayPieza(inicio)) {
-                        //if (!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon"))
-                        if (!tablero.hayPiezasEntre(movi.jugada(jugadadw, tablero))/*&&!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")*/) {
-                            if (tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
-                                if (tablero.hayPieza(fin) && !movi.jugada(jugadadw, tablero).esDiagonal()) {
-                                    tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
-                                    tablero.quitaPieza(inicio);
-
-                                }else if (movi.jugada(jugadadw, tablero).esDiagonal()&& tablero.hayPieza(fin)) {
-                                    System.out.println("movimiento no valido");
-                                } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin) && movi.jugada(jugadadw, tablero).esDiagonal()) {
-                                    tablero.quitaPieza(fin);
-                                    tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
-                                    tablero.quitaPieza(inicio);
-
-                                } else
-                                    System.out.println("movimiento no valido");
-                            } else {
-                                if (tablero.hayPieza(fin)) {
-                                    tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
-                                    tablero.quitaPieza(inicio);
-                                } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
-                                    tablero.quitaPieza(fin);
-                                    tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
-                                    tablero.quitaPieza(inicio);
-                                } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin)) {
-                                    if (movi.jugada(jugadadw, tablero).esDiagonal()) {
+            if (!turn){
+                tablero.pintarTablero();
+                System.out.println("Turno Blancas");
+                System.out.println("jugada");
+                String jugadadw = scan.nextLine();
+                if (movi.jugada(jugadadw, tablero) != null) {
+                    Posicion inicio = movi.jugada(jugadadw, tablero).getPosInicio();
+                    Posicion fin = movi.jugada(jugadadw, tablero).getPosFinal();
+                    Pieza aux = tablero.DevolverPieza(inicio);
+                    if (aux.validoMovimiento(movi.jugada(jugadadw, tablero))) {
+                        if (!tablero.hayPieza(inicio) && !tablero.DevolverPieza(inicio).getColor()) {
+                            //if (!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon"))
+                            if (!tablero.hayPiezasEntre(movi.jugada(jugadadw, tablero))/*&&!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")*/) {
+                                if (tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
+                                    if (tablero.hayPieza(fin) && !movi.jugada(jugadadw, tablero).esDiagonal()) {
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=true;
+                                    }else if (movi.jugada(jugadadw, tablero).esDiagonal()&& tablero.hayPieza(fin)) {
+                                        System.out.println("movimiento no valido");
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin) && movi.jugada(jugadadw, tablero).esDiagonal()) {
                                         tablero.quitaPieza(fin);
                                         tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
                                         tablero.quitaPieza(inicio);
-
-                                    }
-                                } else
-                                    System.out.println("movimiento no valido");
-                            }
+                                        turn=true;
+                                    } else
+                                        System.out.println("movimiento no valido");
+                                } else {
+                                    if (tablero.hayPieza(fin)) {
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=true;
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
+                                        tablero.quitaPieza(fin);
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=true;
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin)) {
+                                        if (movi.jugada(jugadadw, tablero).esDiagonal()) {
+                                            tablero.quitaPieza(fin);
+                                            tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                            tablero.quitaPieza(inicio);
+                                            turn=true;
+                                        }
+                                    } else
+                                        System.out.println("movimiento no valido");
+                                }
+                            } else
+                                System.out.println("movimiento no valido");
                         } else
-                            System.out.println("movimiento no valido");
+                            System.out.println("no valido");
                     } else
                         System.out.println("no valido");
-                } else
-                    System.out.println("no valido");
-                turno++;
+                    turno++;
+                }
+            }else{
+                tablero.pintarTablero();
+                System.out.println("Turno Negras");
+                System.out.println("jugada");
+                String jugadadw = scan.nextLine();
+                if (movi.jugada(jugadadw, tablero) != null) {
+                    Posicion inicio = movi.jugada(jugadadw, tablero).getPosInicio();
+                    Posicion fin = movi.jugada(jugadadw, tablero).getPosFinal();
+                    Pieza aux = tablero.DevolverPieza(inicio);
+                    if (aux.validoMovimiento(movi.jugada(jugadadw, tablero))) {
+                        if (!tablero.hayPieza(inicio) && tablero.DevolverPieza(inicio).getColor()) {
+                            //if (!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon"))
+                            if (!tablero.hayPiezasEntre(movi.jugada(jugadadw, tablero))/*&&!tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")*/) {
+                                if (tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
+                                    if (tablero.hayPieza(fin) && !movi.jugada(jugadadw, tablero).esDiagonal()) {
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=false;
+                                    }else if (movi.jugada(jugadadw, tablero).esDiagonal()&& tablero.hayPieza(fin)) {
+                                        System.out.println("movimiento no valido");
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin) && movi.jugada(jugadadw, tablero).esDiagonal()) {
+                                        tablero.quitaPieza(fin);
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=false;
+                                    } else
+                                        System.out.println("movimiento no valido");
+                                } else {
+                                    if (tablero.hayPieza(fin)) {
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=false;
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.DevolverPieza(inicio).getClass().getSimpleName().equalsIgnoreCase("Peon")) {
+                                        tablero.quitaPieza(fin);
+                                        tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                        tablero.quitaPieza(inicio);
+                                        turn=false;
+                                    } else if (tablero.DevolverPieza(inicio).getColor() != tablero.DevolverPieza(fin).getColor() && !tablero.hayPieza(fin)) {
+                                        if (movi.jugada(jugadadw, tablero).esDiagonal()) {
+                                            tablero.quitaPieza(fin);
+                                            tablero.ponPieza(tablero.DevolverPieza(inicio), fin);
+                                            tablero.quitaPieza(inicio);
+                                            turn=false;
+                                        }
+                                    } else
+                                        System.out.println("movimiento no valido");
+                                }
+                            } else
+                                System.out.println("movimiento no valido");
+                        } else
+                            System.out.println("no valido");
+                    } else
+                        System.out.println("no valido");
+                    turno++;
+                }
             }
         }
     }
